35db168cf0f7374dcb79ceb88298571274c1bb1e
gomjiyun <rlawldbs7770@gmail.com>
2022-06-04 02:00:09 +0900

    beginning

---
+++ src/main/java/com/example/restaurant_list/RestaurantListApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurant_list/RestaurantListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
